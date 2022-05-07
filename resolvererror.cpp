a1f06d99c6aaa23dc6fd62ea31a0b4852ad604da
Martin Di Paola <martinp.dipaola@gmail.com>
2022-05-06 23:38:30 -0300

    Excepción para encapsular errores del Resolver
    getaddrinfo() no usa errno para retornar errores sino que usa otro
    mecanismo, oculto detrás de gai_strerror().
    
    Esta excepción ResolverError encapsula dicho error.

---
+++ resolvererror.cpp
@@ -0,0 +1,11 @@
+#include "resolvererror.h"
+
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+ResolverError::ResolverError(int gai_errno) : gai_errno(gai_errno) { }
+
+const char* ResolverError::what() noexcept {
+    return gai_strerror(gai_errno);
+}
---
+++ resolvererror.h
@@ -0,0 +1,17 @@
+#ifndef RESOLVER_ERROR_H
+#define RESOLVER_ERROR_H
+
+#include <exception>
+
+/*
+ * Clase que encapsula un "gai" error. Vease getaddrinfo()
+ * */
+class ResolverError : public std::exception {
+    int gai_errno;
+
+    public:
+    ResolverError(int gai_errno);
+
+    const char* what() noexcept;
+};
+#endif
